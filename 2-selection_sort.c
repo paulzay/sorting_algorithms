142b67a99af7b002c727f3f7f3b815ef2b04ced0
Paul Ogolla <paulotieno2@gmail.com>
2024-03-05 22:54:31 +0300

    select sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+
+/**
+ * selection_sort - selection sort function
+ * @array: pointr to array
+ * @size: size of array
+ * Return: nothing
+*/
+void selection_sort(int *array, size_t size)
+{
+	unsigned int i, j;
+	unsigned int minm;
+	int temp;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		minm = i;
+		for (j = i + 1; j < size; ++j)
+		{
+			if (array[j] < array[minm])
+			{
+				minm = j;
+			}
+		}
+		if (minm != i)
+		{
+			temp = array[i];
+			array[i] = array[minm];
+			array[minm] = temp;
+		}
+		print_array(array, size);
+	}
+}
